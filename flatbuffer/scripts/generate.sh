2f6cea99e0bbbfddd688e951f14ddf8d4b79b605
Matthew Nikkel <nikkelma@gmail.com>
2019-09-07 18:00:09 -0400

    Add flatbuffer schema and binaries

---
+++ flatbuffer/scripts/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Remove old generated files
+rm -rf ./flat
+
+# Generate go files from flatbuffer schema
+./flatbuffer/bin/flatc --go ./flatbuffer/schema/rlbot.fbs
+
+# Move generated files to intended folder
+mv ./rlbot/flat ./flat
+
+# Clean up intermediate folder
+rm -r ./rlbot
