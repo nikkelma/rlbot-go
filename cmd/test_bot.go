39095b08f30a7c33c9b79e3ec6c52d62f919cd12
Matthew Nikkel <nikkelma@gmail.com>
2019-09-13 12:19:24 -0400

    Implement initial ByteBuffer return value functions

--- cmd/test_bot.go
+++ cmd/test_bot.go
@@ -5,54 +5,53 @@ import (
 	"github.com/nikkelma/rlbot-go/native"
 
 	"fmt"
-	"os"
-	"os/signal"
+	// "os"
+	// "os/signal"
 	"time"
 )
 
 func main() {
 	bridge, err := native.NewBridge()
-	defer bridge.Close()
-
 	if err != nil {
 		fmt.Println("NewBridge error")
 		fmt.Println(err)
 		return
 	}
+	defer bridge.Close()
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt)
+	// signal.Notify(c, os.Kill)
 
 	init, err := bridge.IsInitialized()
 
 	for {
+		if err != nil {
+			fmt.Println("IsInitialized error")
+			fmt.Println(err)
+			return
+		}
 		if init {
 			break
+		} else {
+			fmt.Println("Not initialized yet...")
 		}
-		<-time.After(1 * time.Second)
+		<-time.After(2 * time.Second)
 		init, err = bridge.IsInitialized()
 	}
 
-	fieldInfo, err := bridge.UpdateFieldInfo()
+	fmt.Println("Initialized!")
+
+	matchSettings, err := bridge.GetMatchSettings()
 	if err != nil {
-		fmt.Println("UpdateFieldInfo error")
+		fmt.Println("GetMatchSettings error")
 		fmt.Println(err)
 		return
 	}
 
-	boostPadLength := fieldInfo.BoostPadsLength()
-	if boostPadLength == 0 {
-		fmt.Println("BoostPads length is 0")
-		return
-	}
-	boostPad := &flat.BoostPad{}
-	success := fieldInfo.BoostPads(boostPad, 0)
-	if !success {
-		fmt.Println("BoostPads error")
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("BoostPad")
-	fmt.Println(boostPad.IsFullBoost())
+	gameModeStr := flat.EnumNamesGameMode[int(matchSettings.GameMode())]
+	gameMapStr := flat.EnumNamesGameMap[int(matchSettings.GameMap())]
+
+	fmt.Printf("GameMode: %v\n", gameModeStr)
+	fmt.Printf("GameMap: %v\n", gameMapStr)
 }
--- native/bridge_windows.go
+++ native/bridge_windows.go
@@ -14,30 +14,37 @@ import (
 )
 
 type windowsProc struct {
-	*sync.Mutex
+	sync.Mutex
 	*windows.Proc
 }
+
 func createWindowsProc(dll *windows.DLL, procName string) *windowsProc {
 	return &windowsProc{
 		Proc: dll.MustFindProc(procName),
 	}
 }
 
-func newBridge() (bridge Bridge, err error) {
+func newBridge() (Bridge, error) {
 	var dllName string = getDllName()
 	fmt.Println(dllName)
+
 	// TODO - confirm avoidance of DLL pre-loading attacks
 	rlBotInterfaceDll, err := windows.LoadDLL(dllName)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO - only load Proc when needed
-	_bridge := &bridgeWindows{
+	if rlBotInterfaceDll == nil {
+		err = fmt.Errorf("nil rlBotInterfaceDll error")
+		return nil, err
+	}
+	fmt.Println(rlBotInterfaceDll)
+
+	bridge := &bridgeWindows{
 		rlBotInterfaceDll: rlBotInterfaceDll,
 		freeProc:          createWindowsProc(rlBotInterfaceDll, "Free"),
 	}
-	return _bridge, nil
+	return bridge, nil
 }
 
 // TODO - determine if locks are needed, refactor to struct to allow for locks
@@ -60,6 +67,11 @@ type bridgeWindows struct {
 }
 
 func (b *bridgeWindows) Close() error {
+	fmt.Println("bridgeWindows Close")
+
+	if b.rlBotInterfaceDll == nil {
+		return fmt.Errorf("nil rlBotInterfaceDll")
+	}
 	return b.rlBotInterfaceDll.Release()
 }
 
@@ -81,64 +93,99 @@ func (b *bridgeWindows) IsInitialized() (bool, error) {
 
 // GameFunctions/BallPrediction.hpp
 func (b *bridgeWindows) GetBallPrediction() (*flat.BallPrediction, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Not implemented")
 }
 
 // GameFunctions/GameFunctions.hpp
 func (b *bridgeWindows) SetGameState(*flat.DesiredGameState) error {
-	return nil
+	return fmt.Errorf("Not implemented")
 }
+
 func (b *bridgeWindows) StartMatch(*flat.MatchSettings) error {
-	return nil
+	return fmt.Errorf("Not implemented")
 }
 
 // GameFunctions/GamePacket.hpp
 func (b *bridgeWindows) UpdateFieldInfo() (*flat.FieldInfo, error) {
 	if b.updateFieldInfoFlatbufferProc == nil {
-		b.updateFieldInfoFlatbufferProc = createWindowsProc(b.rlBotInterfaceDll, "UpdateLiveDataPacketFlatbuffer")
+		b.updateFieldInfoFlatbufferProc = createWindowsProc(b.rlBotInterfaceDll, "UpdateFieldInfoFlatbuffer")
 	}
 
 	b.updateFieldInfoFlatbufferProc.Lock()
 	defer b.updateFieldInfoFlatbufferProc.Unlock()
+	ptr, size, errno := b.updateFieldInfoFlatbufferProc.Call()
 
-	res, _, errno := b.updateFieldInfoFlatbufferProc.Call()
 	if errno != syscall.Errno(0) {
-		return nil, errno
+		return nil, fmt.Errorf("UpdateFieldInfo error: %v", errno)
 	}
 
-	fmt.Println(res)
+	fieldInfoBytes := make([]byte, size)
+	for i := 0; i < int(size); i++ {
+		fieldInfoBytes[i] = *(*byte)(unsafe.Pointer(ptr + uintptr(i)))
+	}
 
-	// myvar := *(*C.ByteBuffer)(unsafe.Pointer(res))
-	// fieldInfo := flat.GetRootAsFieldInfo(*myvar.ptr, myvar.size)
+	_, _, errno = b.freeProc.Call(ptr)
+	if errno != syscall.Errno(0) {
+		return nil, fmt.Errorf("Free error: %v", errno)
+	}
 
-	// fmt.Println(fieldInfo)
+	fieldInfo := flat.GetRootAsFieldInfo(fieldInfoBytes, 0)
 
-	return nil, nil
+	return fieldInfo, nil
 }
+
 func (b *bridgeWindows) UpdateLiveDataPacket() (*flat.GameTickPacket, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Not implemented")
 }
+
 func (b *bridgeWindows) UpdateRigidBodyTick() (*flat.RigidBodyTick, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Not implemented")
 }
+
 func (b *bridgeWindows) GetMatchSettings() (*flat.MatchSettings, error) {
-	return nil, nil
+	if b.getMatchSettingsProc == nil {
+		b.getMatchSettingsProc = createWindowsProc(b.rlBotInterfaceDll, "GetMatchSettings")
+	}
+
+	b.getMatchSettingsProc.Lock()
+	defer b.getMatchSettingsProc.Unlock()
+	ptr, size, errno := b.getMatchSettingsProc.Call()
+
+	if errno != syscall.Errno(0) {
+		return nil, fmt.Errorf("GetMatchSettings error: %v", errno)
+	}
+
+	matchSettingsBytes := make([]byte, size)
+	for i := 0; i < int(size); i++ {
+		matchSettingsBytes[i] = *(*byte)(unsafe.Pointer(ptr + uintptr(i)))
+	}
+
+	_, _, errno = b.freeProc.Call(ptr)
+	if errno != syscall.Errno(0) {
+		return nil, fmt.Errorf("Free error: %v", errno)
+	}
+
+	matchSettings := flat.GetRootAsMatchSettings(matchSettingsBytes, 0)
+
+	return matchSettings, nil
 }
 
 // GameFunctions/PlayerInfo.hpp
 func (b *bridgeWindows) SendQuickChat(*flat.QuickChat) error {
-	return nil
+	return fmt.Errorf("Not implemented")
 }
+
 func (b *bridgeWindows) ReceiveChat() (*flat.QuickChatMessages, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Not implemented")
 }
+
 func (b *bridgeWindows) UpdatePlayerInput(*flat.PlayerInput) error {
-	return nil
+	return fmt.Errorf("Not implemented")
 }
 
 // RenderFunctions/RenderFunctions.hpp
 func (b *bridgeWindows) RenderGroup(*flat.RenderGroup) error {
-	return nil
+	return fmt.Errorf("Not implemented")
 }
 
 // ensure *bridgeWindows satisfies Bridge interface
